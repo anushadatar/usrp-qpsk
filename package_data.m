8c569256a5f1f3d6aa9addd9b1815c60fad683fd
Mark Goldwater <mgoldwater@olin.edu>
2021-02-09 21:52:48 -0500

    Adding function to package the data in format usable by usrp

---
+++ package_data.m
@@ -0,0 +1,32 @@
+function package_data(N, symbol_period, header_size)
+    % Format a transmit file ('tx.dat') for the usrp to send.
+    % Input : N = Number of random data bits to generate
+    %         symbol_period = Number of samples between rectangular pulse
+    %                         symbols
+    % Output: 'tx.dat' = File saved with data ready to be transmitted via
+    %                    usrp
+   
+    % GENERATE BITS
+    I_raw = sign(randn(N,1))';
+    Q_raw = sign(randn(N,1))';
+    I = add_known_bits(header_size, I_raw)';
+    Q = add_known_bits(header_size, Q_raw)';
+    bits = I + 1i*Q;
+
+    % CREATE DATA
+    pulse = ones(symbol_period, 1);
+    x = zeros(symbol_period*length(bits),1);
+    x(1:symbol_period:end) = bits;
+    x_tx = conv(pulse, x);
+    
+    % CREATE ARRAY FOR FILE
+    x_tx = [zeros(100000, 1); x_tx; zeros(100000, 1)];
+    tmp = zeros(length(x_tx)*2, 1);
+    tmp(1:2:end) = real(x_tx);
+    tmp(2:2:end) = imag(x_tx);
+    
+    % SAVE DATA TO FILE
+    f = fopen('tx.dat', 'wb');
+    fwrite(f, tmp, 'float32');
+    fclose(f);
+end
