ce27c81c61456d9610398fb5c1546758166fc59e
Anusha Datar <anushadatar@gmail.com>
2021-02-10 09:53:08 -0500

    Added unpack data file.

---
+++ unpack_data.m
@@ -0,0 +1,17 @@
+function processed_data = unpack_data(pulse_size, header_size, rotated_data)
+    % Take the rotated complex data, remove the known bits, and decode it
+    % into a single vector of complex values.
+    % Input:  pulse_size     = 
+    %         header_size    = 
+    %         rotated_data   =   
+    % Output: processed_data = 
+    % Remove known data bits.
+    trimmed_data = rotated_data(header_size + 1:end);
+    % Decode bits.
+    for i=pulse_size/2:pulse_size*len(trimmed_data)
+        bits_i = [bits_i real(trimmed_data(i))];
+        bits_q = [bits_q imag(trimmed_data(i))];
+    end
+    % Return the values as a single complex vector.
+    processed_data = bits_i + 1j*bits_q;
+end
