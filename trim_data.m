0372a66ca4075cb3c8b418b636d6bcf8e029b8f7
Anusha Datar <anushadatar@gmail.com>
2021-02-10 09:36:01 -0500

    corrected inconsistencies in trim data header, still needs docs

--- trim_data.m
+++ trim_data.m
@@ -1,8 +1,8 @@
-function  [trimmed_data, known_sig] = trim_data(received_data, pulse_size, header_size)
+function  [trimmed_data] = trim_data(pulse_size, header_size, received_data)
     % TODO Docstring
     % Find the beginning of the known data in the header 
-    known_sig = ones(header_size.*pulse_size,1)';
-    [xCorr,lags] = xcorr(received_data,known_sig);    
+    known_data = ones(header_size.*pulse_size,1)';
+    [xCorr,lags] = xcorr(received_data,known_data);    
     [~,I] = max(abs(xCorr));
     max_time = lags(I) + header_size*pulse_size;
     trimmed_data = received_data(max_time:end); 
