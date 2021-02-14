dd1e01130d2aa07d094bdfc4d7517b3227285275
Anusha Datar <anushadatar@gmail.com>
2021-02-14 01:25:43 -0500

    added docstrings to functions that lacked them

--- process_raw_data_file.m
+++ process_raw_data_file.m
@@ -1,5 +1,8 @@
 function rx = process_raw_data_file(rx_fname)
-    % TODO docstring
+    % Convert the raw data file saved by the receiver into an array
+    % of complex numbers.
+    % Input: rx_fname = The name of the file to process.
+    % Output: rx      = The array of complex number read from the file.
     raw_data_file = fopen(rx_fname, 'r');
     raw_data_tmp = fread(raw_data_file,'float32');
     fclose(raw_data_file);
--- trim_data.m
+++ trim_data.m
@@ -1,6 +1,15 @@
 function  [trimmed_data] = trim_data(pulse_size, header_size, data_size, received_data)
-    % TODO Docstring
-    % Find the beginning of the known data in the header 
+    % Find the beginning of the transmitted signal in the received data
+    % and trim the data to start at that point and end following the
+    % number of bits in its length. This assumes the receiver already
+    % knows the transmitter's pulse size, header size, and data size.
+    % Inputs: pulse_size    = The size of the pulse
+    %         header_size   = The number of known bits at the beginning of
+    %                         the signal.
+    %         data_size     = The number of elements in the data, not
+    %                         including the header.
+    %         received_data = The received data to trim.
+    % Outputs: trimmed_data = The section of the data including the signal.
     known_data = ones(header_size.*pulse_size,1)';
     [xCorr,lags] = xcorr(received_data,known_data);    
     [~,I] = max(abs(xCorr));
